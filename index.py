19a4a220cc44437ba519f017df7f522ed7531fcd
Luz Meriyen Quispe Comun <2024141050@unh.edu.pe>
2026-06-09 11:26:27 -0500

    cambio liuz

--- index.py
+++ index.py
@@ -1,7 +1,18 @@
 echo("hola mundo")
+<<<<<<< HEAD
 echo("tuli broca")
 print("hola mundo")
 print("tuli broca")
 print("hpolaaaa")
 print("HELLOOO")
 print("HELLOOO")
+=======
+echo("hola aparco")
+echo("hlloe)")
+echo("hola mundo")
+echo("hola aparco")
+echo("hlloe)")
+echo("hola mundo")
+echo("hola aparco")
+echo("hlloe)")
+>>>>>>> e5abdb6 (cambio liuz)
